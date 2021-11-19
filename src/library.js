f48df2ee677b01ea8f7ea6c2fcf41f7af7b55798
ms-anastasia <nastyakorn0@gmail.com>
2021-11-19 22:36:15 +0200

    adds library page

---
+++ src/library.js
@@ -0,0 +1,15 @@
+// основной скрипт
+
+// файл стилей
+import './sass/main.scss';
+
+// скрипт взаимодействия с API
+import './js/apiService';
+import './js/cardFetch';
+import './js/searchFilms';
+// файл шаблонизатор страницы
+import listCards from './templates/listCards.hbs';
+
+// спиннер
+import { Spinner } from 'spin.js';
+
