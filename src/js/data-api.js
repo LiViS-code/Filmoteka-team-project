ad85fcb0404c8dd123177fc246571be5da8b57e8
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2021-11-19 16:25:53 +0200

    del dublicate

--- src/js/data-api.js
+++
@@ -1,6 +0,0 @@
-// API key: b7df999202e1c3618d01db23ce0076f0
-// example API request: https://api.themoviedb.org/3/movie/550?api_key=b7df999202e1c3618d01db23ce0076f0
-
-export const KEY = 'b7df999202e1c3618d01db23ce0076f0';
-export const BASEURL = 'https://api.themoviedb.org/3';
-export const URL = `${BASEURL}/movie/`;
