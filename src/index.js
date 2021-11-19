7d1fb77524d49958dc3f3a5f20561018c8db6b15
Nadiia-Myronenko <nadegda.burina@gmail.com>
2021-11-19 21:08:05 +0200

    при нажатии на myLibrary меняется фон

--- src/index.js
+++ src/index.js
@@ -13,3 +13,21 @@ import listCards from './templates/listCards.hbs';
 // спиннер
 import { Spinner } from 'spin.js';
 
+//изменение фона по клику
+
+const myLibraryBtn = document.querySelector('[data-action="my-library-rendering"]');
+const headerEl = document.querySelector('header');
+
+myLibraryBtn.addEventListener('click', onMyLibraryClick);
+
+function onMyLibraryClick() {
+    bgImageChange('home-header', 'library-header');
+}
+
+function bgImageChange(oldBg, newBg) {
+    if (headerEl.classList.contains(oldBg)) {
+        console.log('contains!')
+        headerEl.classList.remove(oldBg);
+    }
+    headerEl.classList.add(newBg);
+}
