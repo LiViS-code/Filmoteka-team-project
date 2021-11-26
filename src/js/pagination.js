1b987381963befff8ac58abbf69d105a382a0949
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2021-11-26 18:01:15 +0200

    убрал не используемый шаблон

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -3,29 +3,15 @@ import refs from './Refs';
 import { render } from './cardFetch';
 import { FilmSearchByWordPagination } from './searchFilms';
 
-export function addPagination(totalItems, itemsPerPage, page) {
+export default addPagination;
+
+function addPagination(totalItems, itemsPerPage, page) {
   const optionPagination = {
     totalItems,
     itemsPerPage,
     visiblePages: 5,
     centerAlign: true,
     usageStatistics: false,
-    template: {
-      page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-      currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-      moveButton:
-        '<a href="#" class="tui-page-btn tui-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
-        '</a>',
-      disabledMoveButton:
-        '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
-        '</span>',
-      moreButton:
-        '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-        '<span class="tui-ico-ellip">...</span>' +
-        '</a>',
-    },
   };
 
   const pagination = new Pagination(refs.paginationContainer, optionPagination);
@@ -35,6 +21,6 @@ export function addPagination(totalItems, itemsPerPage, page) {
   pagination.on('beforeMove', event => {
     const searchedFilm = localStorage.getItem('searched');
     if (searchedFilm === '') return render(event.page);
-    FilmSearchByWordPagination(searchedFilm, event.page);
+    return FilmSearchByWordPagination(searchedFilm, event.page);
   });
 }
