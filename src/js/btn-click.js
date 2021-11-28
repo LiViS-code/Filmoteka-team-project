1334adaaf22d3360830a6833fc10688681baf96a
R0DlON <rodionprokopchenkoo@gmail.com>
2021-11-28 16:32:53 +0300

    добавил правильный рендер при клике на ADD TO QUEUED, добавил отрисовку пагинации и добавил правильную отрисовку при переходе на другую страничку пагинации

--- src/js/btn-click.js
+++ src/js/btn-click.js
@@ -1,14 +1,20 @@
 import { watchedBtn, queueBtn, listWatchedFilms, listQueuedFilms } from './refs';
-import { checkPaginationForWatched, ckechQueueFilms, getIdFromLocalStorage } from './library';
+import { checkPaginationForLibrary, getIdFromLocalStorage } from './library';
 
-queueBtn.addEventListener('click', () => btnClick('remove', 'add', ckechQueueFilms));
-watchedBtn.addEventListener('click', () => btnClick('add', 'remove', checkPaginationForWatched));
+queueBtn.addEventListener('click', () => btnClick('remove', 'add', checkPaginationForLibrary));
+watchedBtn.addEventListener('click', () => btnClick('add', 'remove', checkPaginationForLibrary));
 
 function btnClick(action1, action2, func) {
   watchedBtn.classList[action1]('btn-current');
   queueBtn.classList[action2]('btn-current');
   listWatchedFilms.classList[action2]('visually-hidden');
   listQueuedFilms.classList[action1]('visually-hidden');
+
   let updatedLocaleStorageWatched = getIdFromLocalStorage('watchedFilms');
-  func(updatedLocaleStorageWatched);
+  let updatedLocaleStorageQueued = getIdFromLocalStorage('queuedFilms');
+  if (watchedBtn.classList.contains('btn-current')) {
+    func(updatedLocaleStorageWatched);
+    return;
+  }
+  func(updatedLocaleStorageQueued);
 }
