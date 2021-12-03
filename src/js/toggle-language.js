76447fe5faebbba02b2913e944850a4f28a89063
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2021-12-04 00:13:30 +0200

    перезагрузка страницы

--- src/js/toggle-language.js
+++ src/js/toggle-language.js
@@ -1,5 +1,6 @@
 import { saveSelectLanguage } from './language-interface';
 
+// сменить активную кнопку
 export function onLanguageSelect(e) {
   const attr = 'data-btn-lng';
   const classActive = 'toggle-language__btn--active';
@@ -16,6 +17,12 @@ export function onLanguageSelect(e) {
     if (btn.classList.contains(classActive)) btn.classList.toggle(classActive);
     if (btn.getAttribute(attr) === selectLanguage) btn.classList.add(classActive);
   });
+
+  changeLanguageInterface(selectLanguage);
+}
+
+function changeLanguageInterface(lang) {
+  // alert(`меняем весь интерфейс на: ${lang}`);
 
   // перезагрузить страницу
   document.location.reload();
